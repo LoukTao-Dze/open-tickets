4c4eac91573068396a48b4b9ba21007738dd599b
LoukTao-Dze <looktaotsk@gmail.com>
2026-09-21 09:42:58 +0700

    feat: enhance ticket management by adding meta data support and updating ticket form

--- package/frontend/src/app/pages/kanban/kanban.component.ts
+++ package/frontend/src/app/pages/kanban/kanban.component.ts
@@ -168,7 +168,6 @@ export class KanbanComponent implements OnInit {
     if (!targetColumn) {
       return;
     }
-
     this.kanbanApi.createTicket(value).subscribe({
       next: (ticket) => {
         targetColumn.tickets.push(ticket);
